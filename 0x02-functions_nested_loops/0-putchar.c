e6a8097e273d43bf641f571c0e59f43ada6eaeb7
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-06-20 11:32:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *main - program starting point
+  *Description: print _putchar to the screen
+  *Return: Always 0
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
