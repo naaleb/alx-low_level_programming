c930303465ce182de0efb5acf8abbae2e71fba30
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-06-21 21:48:12 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; a++)
 				{
-					for (a >= 2 && b >= 4)
+					if (a >= 2 && b >= 4)
 						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
