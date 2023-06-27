d98a86a1bde82de33ee729308cd7e3653dababf2
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-06-27 06:55:15 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,14 @@
 void rev_string(char *s)
 {
 	int i = 0;
+	int length = 0;
 
-	while (s[i] != '\0')
+	while (s[length] != '\0')
+	{
 		i++;
+	}
 
-	for (i = i - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
