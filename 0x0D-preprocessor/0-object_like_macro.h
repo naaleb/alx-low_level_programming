5ff23bd7295f1f2a7f506eaeae146d9532100901
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-07-17 16:15:22 -0700

    Task 0 to 2

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+  *Macro named SIZE that represent the token 1024
+  *
+  */
+
+#define SIZE 1024
+
+#endif /* OBJECT_LIKE_MACRO_H */
