cee6e4d0019263d303c4121e7b02ee1d20c6468e
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-07-17 19:01:15 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *dog.h
+  *dog attribute include name, age and owner
+  *struct dog - representing a dog with it attributes
+  *@name: dog name
+  *@age: doge age
+  *@owner: dog owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
