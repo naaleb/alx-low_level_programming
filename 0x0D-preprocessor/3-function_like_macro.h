362ed0fe40e1c04e261412d8944f2971fcbf8082
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-07-17 17:54:41 -0700

    Preprocessor tasked 3&4

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+  * 3-function_like_macro.h
+  * macro ABS(x) that computes the absolute value of a number x
+  * @x: number for which absolute value is calculated
+  */
+#define ABS(x)((x) < 0 ? -(x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+  *4-sum.h
+  *@x: First number to be added
+  *@y: Second number to be added
+  */
+
+#define SUM(x, y)((x) + (y))
+
+#endif
