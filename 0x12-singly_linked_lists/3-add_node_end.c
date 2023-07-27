0c132d26950519e09a4865bddebee156dc6b0e9d
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-07-27 16:30:49 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,62 +10,34 @@
  * Return: address of the new element, or NULL if it failed
  */
 
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	if (str == NULL)
-	{
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
 		return (NULL);
-	}
-
-	new_node = (list_t *)malloc(sizeof(list_t));
-
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
 
 	new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new_node;
-	} else
-	{
-		list_t *last_node = *head;
-
-		while (last_node->next != NULL)
-		{
-			last_node = last_node->next;
-		}
-
-		last_node->next = new_node;
+		return (new_node);
 	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new_node;
 
 	return (new_node);
 }
 
-/* Function to free the memory occupied by the linked list */
-void free_list(list_t *head)
-{
-	list_t *current = head;
-
-	while (current != NULL)
-	{
-		list_t *temp = current;
-
-		current = current->next;
-		free(temp->str);
-		free(temp);
-	}
-}
