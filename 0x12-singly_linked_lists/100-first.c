2fc3f9baa9c9c83d061cd9421634528334af3563
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-07-27 18:04:31 -0700

    advance 2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
-#include "lists.h"
 #include <stdio.h>
 
+void print_before_main(void) __attribute__ ((constructor));
+
 /**
- * print_lines_before_main - Execute before main function.
+ * print_before_main - Execute before main function.
  * return: No return
  */
-void print_lines_before_main(void)
+void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
