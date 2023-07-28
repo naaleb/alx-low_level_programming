dd0e96b0dec70d97a11329b08b5a7584c8af249a
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-07-27 17:39:07 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,22 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_lines_before_main - Prints lines before main function is executed.
+ * print_lines_before_main - Execute before main function.
+ * return: No return
  */
 void print_lines_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
-
-/**
- * main - Program starting point.
- * Return: Always 0 (success)
- */
-/**
- * int main(void)
- *{
- *	print_lines_before_main();
- *	return (0);
- *}
- */
